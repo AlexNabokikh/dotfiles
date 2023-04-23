d8d2ff794713ad455387377f7822017bfc383f17
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-04-23 15:16:17 +0200

    feat(nvim): added mason config

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        ansible_language_server,
+        ansible_lint,
+        bash_language_server,
+        black,
+        dockerfile_language_server,
+        flake8,
+        goimports,
+        golangci_lint,
+        golangci_lint_langserver,
+        grammarly_languageserver,
+        hadolint,
+        helm_ls,
+        isort,
+        json_lsp,
+        lua_language_server,
+        markdownlint,
+        pyright,
+        ruff,
+        shellcheck,
+        shfmt,
+        stylua,
+        terraform_ls,
+        tflint,
+        yaml_language_server,
+        yamlfix,
+        yamlfmt,
+        yamllint,
+      },
+    }
+  },
+}
