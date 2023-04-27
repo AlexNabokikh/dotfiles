841afc2b4fc4dd4a7f3f550f5637099615ed0835
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-04-27 09:30:11 +0200

    feat(nvim): added adjusted formatting config

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -7,23 +7,27 @@ return {
         sources = {
           -- formatters
           nls.builtins.formatting.black,
+          nls.builtins.formatting.prettier.with {
+            filetypes = {
+              "yaml",
+              "yaml.ansible",
+              "yaml.docker-compose",
+              "yaml.kubernetes",
+              "yaml.terraform",
+            },
+          },
           nls.builtins.formatting.goimports,
           nls.builtins.formatting.isort,
           nls.builtins.formatting.markdownlint,
           nls.builtins.formatting.shfmt,
           nls.builtins.formatting.stylua,
-          nls.builtins.formatting.yamlfix,
-          nls.builtins.formatting.yamlfmt,
           -- linters
-          -- nls.builtins.diagnostics.ansible-lint,
           nls.builtins.diagnostics.flake8,
           nls.builtins.diagnostics.golangci_lint,
           nls.builtins.diagnostics.hadolint,
           nls.builtins.diagnostics.markdownlint,
           nls.builtins.diagnostics.ruff,
           nls.builtins.diagnostics.shellcheck,
-          -- nls.builtins.diagnostics.tflint,
-          nls.builtins.diagnostics.yamllint,
         },
       }
     end
