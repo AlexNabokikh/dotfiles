556c01bc08d54d83498bcdb14b7355cd444b7312
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-04-27 19:11:48 +0200

    feat(nvim): removed cspell

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -5,25 +5,6 @@ return {
     return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
-        -- code actions
-        nls.builtins.code_actions.cspell.with({
-          extra_args = { "--config", "~/.config/nvim/utils/cspell.json" },
-          config = {
-            find_json = function()
-              return vim.fn.expand("~/.config/nvim/utils/cspell.json")
-            end,
-            on_success = function()
-              os.execute(
-                "cat ~/.config/nvim/utils/cspell.json | jq -S '.words |= sort' | tee ~/.config/nvim/utils/cspell.json > /dev/null"
-              )
-            end,
-          },
-          filetypes = {
-            "markdown",
-            "text",
-            "txt",
-          },
-        }),
         -- formatters
         nls.builtins.formatting.black,
         nls.builtins.formatting.prettier.with({
@@ -43,7 +24,6 @@ return {
         nls.builtins.formatting.terraform_fmt,
         -- linters
         nls.builtins.diagnostics.ansiblelint,
-        nls.builtins.diagnostics.cspell,
         nls.builtins.diagnostics.flake8,
         nls.builtins.diagnostics.golangci_lint,
         nls.builtins.diagnostics.hadolint,
