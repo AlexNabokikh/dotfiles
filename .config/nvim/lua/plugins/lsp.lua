ce7f9bbbc176fada3c8c35a60ee6bb2198e01d49
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-04-28 16:23:00 +0200

    feat(nvim): added gols config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,6 +21,26 @@ return {
             },
           },
         },
+        gopls = {
+          settings = {
+            gopls = {
+              semanticTokens = true,
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+            },
+          },
+        },
       },
     },
   },
