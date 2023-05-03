7ffa1591e381b68adbce3197024567fa6e2b3c93
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-05-03 22:23:00 +0200

    feat(nvim): added fzf native config

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      hidden = true,
+    },
+  },
+}
