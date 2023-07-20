32d0724d4c48cceb3a47b47f25cd673a52433cb7
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-07-20 19:02:44 +0200

    feat(nvim): removed tmux nav as it doesn't fit the workflow

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,10 +8,3 @@ vim.keymap.set("n", "<leader>fa", function()
 end, { desc = "Find including hidden" })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("t", "<Esc><Esc>", "<Esc>")
-vim.cmd([[
-  noremap <silent> <c-h> :<C-U>TmuxNavigateLeft<cr>
-  noremap <silent> <c-j> :<C-U>TmuxNavigateDown<cr>
-  noremap <silent> <c-k> :<C-U>TmuxNavigateUp<cr>
-  noremap <silent> <c-l> :<C-U>TmuxNavigateRight<cr>
-  noremap <silent> <c-\> :<C-U>TmuxNavigatePrevious<cr>
-]])
