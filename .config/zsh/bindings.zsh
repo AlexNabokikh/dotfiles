9ccbb3e418e8afa6d944b0fbb0b976faa1416b3d
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-10 11:29:00 +0100

    chore(cleanup): removed configs that are migrated to home-manager

--- .config/zsh/bindings.zsh
+++
@@ -1,7 +0,0 @@
-# Set ctrl+f to run cd-to-project
-bindkey -s ^f "cd-to-project\n"
-# Set V to edit command line in vim
-bindkey -M vicmd 'V' edit-command-line
-# Re-Set alt+backspace to delete word
-bindkey '^[^?' backward-kill-word
-
